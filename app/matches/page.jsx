9399b1fefaa29b94fca5d63b03a42cf5276a3c7f
{Jayendra <{jay.bharti2804@gmail.com}>
2025-04-13 16:44:24 +0530

    added matches brackets

--- app/matches/page.jsx
+++ app/matches/page.jsx
@@ -1,6 +1,16 @@
 "use client";
 import { useRouter } from "next/navigation"
-export default function Home(){
-    const router = useRouter();
-    router.push("/matches/live")
+export default function Matches(){
+    return(
+        <div className="flex h-full w-full">
+            <iframe 
+                src="https://challonge.com/futsal_mania_2025_iqlipse/module" 
+                width="100%" 
+                height="100%" 
+                frameBorder="0" 
+                scrolling="auto" 
+                allowtransparency="true"
+            ></iframe>
+        </div>
+    )
 }
