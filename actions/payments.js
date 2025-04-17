256808b4148449ef98cdecac97b556d36fed5fcb
{Jayendra <{jay.bharti2804@gmail.com}>
2025-04-17 17:50:40 +0530

    connect to db

--- actions/payments.js
+++ actions/payments.js
@@ -3,9 +3,11 @@ import Payments from "@/models/payments";
 import User from "@/models/user";
 import Players from "@/models/players";
 import Teams from "@/models/teams";
+import { connectToDB } from "@/utils/database";
 
 export async function GetPayments(){
     
+    await connectToDB();
     const payments = await Payments.find().populate("user");
 
     return JSON.stringify(payments);
@@ -13,6 +15,7 @@ export async function GetPayments(){
 }
 
 export async function UpdatePaymentStatus(paymentId, newStatus){
+    await connectToDB();
     const data = await Payments.findByIdAndUpdate(paymentId,{ status : newStatus })
 
     if(data)return true;
